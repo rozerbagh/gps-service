8160b386a66f8d4fc37a21c4b83e8ac547252d75
rozerAdda247 <bag.rozer@adda247.com>
2023-04-08 07:31:32 +0530

    the new file for mongodb and socket io

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Data = require('./api/models/dataModel')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Data = require("./api/models/dataModel");
 // const request = require('request');
-const dataRouter = require('./api/routes/dataRoute');
+const dataRouter = require("./api/routes/dataRoute");
 
 // m-lab mongo connection
-mongoose.connect('mongodb://adminrozerapp:19Vicky93%408050849022@143.244.128.159:27017/gpsdevice',{ useNewUrlParser : true });
-
+mongoose
+  .connect(
+    "mongodb://adminrozerapp:19Vicky93%408050849022@143.244.128.159:27017/gpsdevice?authMechanism=DEFAULT&authSource=admin&readPreference=primary&appname=lemarchnad&directConnection=true&ssl=false",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((res) => {
+    // console.log(res)
+    console.log("mongodb is connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // mongodb://ambeedev:ambeedev1@ds151293.mlab.com:51293/heroku_gvsq2gkn
 // localhost mongo connection
-// mongoose.connect('mongodb://127.0.0.1:27017/data-test',{ useNewUrlParser: true } )
+// mongoose.connect("mongodb://127.0.0.1:27017/data-test", {
+//   useNewUrlParser: true,
+// });
 
 // use bodyParser
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // cors handling
 app.use((req, res, next) => {
-    // * can be replaced by any http://somthing.com
-    res.header('Access-Control-Allow-Origin','*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );  
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')    
-        return res.status(200).json({});
-    }
-    next();
+  // * can be replaced by any http://somthing.com
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-
 // welcome api on '/' route
-app.get('/',(req,res,next) => {
-    res.status(200).json({
-        message:".....server is running",
-        credit:"powered by -Ambee"
-    })
-})
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: ".....server is running",
+    credit: "powered by -Ambee",
+  });
+});
 
-
-app.use('/api/v1/data', dataRouter);
-
+app.use("/api/v1/data", dataRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,38 @@
-const http = require('http');
-const app = require('./app');
-
 const port = process.env.PORT || 8800;
+const http = require("http");
+const app = require("./app");
+const { Server } = require("socket.io");
 
+console.log("server is running on port : " + port);
 const server = http.createServer(app);
+const io = new Server(server, {
+  // options
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  },
+});
+
+io.on("connection", (socket) => {
+  // ...
+  console.log(":::: wss has been connected ::::", socket);
+  io.sockets.emit("hi", "everyone");
+  io.sockets.emit("data", (data) => {
+    console.log(data);
+  });
+  io.sockets.emit("message", (data) => {
+    console.log(data);
+  });
+  io.sockets.emit("pool", (data) => {
+    console.log(data);
+  });
+  io.sockets.emit("Pool", (data) => {
+    console.log(data);
+  });
+});
+io.on("new_namespace", (namespace) => {
+  console.log(namespace.name);
+});
 
 server.listen(port);
-console.log('server is running on port : '+port )
+// webSocketServer.listen(8800);
