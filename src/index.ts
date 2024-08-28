5fb30741b8f60a19d9d0da04f906731ff867e380
Rozer Bagh <40331263+rozerbagh@users.noreply.github.com>
2024-08-28 14:07:29 +0530

    Typescript version (#18)
    * the new file firebase.json
    
    * new gitignore
    
    * new gitignore
    
    * the conosle.log
    
    * the conflist resolve
    
    * the response has been modified as per the old response
    
    * the build bugs fixed
    
    * new routes
    
    * new routes
    
    * the fetch routes buses
    
    * the fetch routes buses
    
    * fetchRespectiveSchoolsBuses method implemented
    
    * fetchRespectiveSchoolsBuses method implemented
    
    * the login response enhanced
    
    * the refinement of the api and models
    
    * the refinement of the api and models
    
    * the default routes has been chenged
    
    * the default routes has been chenged
    
    * the default routes has been chenged
    
    * the delete busroures api
    
    * the delete busroures api
    
    * the delete busroures api
    
    * the stop-bus event
    
    ---------
    
    Co-authored-by: rozerAdda247 <bag.rozer@adda247.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { createServer } from "http";
+import { createServer, IncomingMessage } from "http";
+import WebSocket, { Server } from "ws";
 import Applications from "./app";
 import { websocketConnection } from "./socket_server";
 const {app, websockets} = Applications
@@ -7,6 +8,41 @@ const HTTP_SERVER_PORT = 3006;
 const httpserver = createServer(app);
 const socketserver = websocketConnection(httpserver);
 websockets(socketserver);
+app.get("/chatevent/stoptrack/:busid/:role", (req, res) => {
+  try{
+    const { busid, role } = req.params;
+    // Trigger a WebSocket event
+    const message = `Stop tracking bus ID: ${busid}`;
+    socketserver.broadcast(
+      JSON.stringify({
+        type: "STOP_BUS_EVENT",
+        payload: {
+          busid: busid,
+          role: role,
+        },
+      })
+    ); // Broadcast message to all WebSocket clients
+    res.send({
+      statusCode: 200,
+      response: {
+        status: true,
+        message: `Broadcasted message: ${message}`,
+        data: null,
+        error: null,
+      },
+    });
+  } catch (error){
+    res.send({
+      statusCode: 500,
+      response: {
+        status: false,
+        message: `Internal Server Error`,
+        data: null,
+        error: null,
+      },
+    });
+  }
+});
 httpserver.listen(HTTP_SERVER_PORT, () => {
   // console.log("server is running on port : " + HTTP_SERVER_PORT);
 });
